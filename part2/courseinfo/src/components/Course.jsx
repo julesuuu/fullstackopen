d6bff4d6bcf04ca919d83a76f03b11e2d8869ef9
Julius De Perio <julesrdeperio@gmail.com>
2025-12-31 15:13:35 +0800

    2.2: Course information step 7

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -3,7 +3,6 @@ const Header = ({course}) => <h1>{course.name}</h1>
 const Content = ({ course }) => {
   const {parts} = course
   console.log('parts', parts)
-  
   return (
     <div>
       {parts.map(part =>
@@ -15,12 +14,24 @@ const Content = ({ course }) => {
   )
 }
 
+const Total = ({ course }) => {
+  const { parts } = course
+  console.log('parts' ,parts)
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises + parts[3].exercises
+  return (
+    <strong>
+      total of {total} exercises
+    </strong>
+  )
+}
+
 const Course = ({ course }) => {
   console.log('course', course)
   return (
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
