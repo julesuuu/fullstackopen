1632149e0ac5f31d62c44a6eaa5304a220b3ae97
Julius De Perio <julesrdeperio@gmail.com>
2025-12-31 18:03:22 +0800

    2.5: Separate module step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -49,9 +49,11 @@ const App = () => {
   return (
     <div>
       <h1>Web development curriculum</h1>
-      {courses.map(course =>  (
+      {
+        courses.map(course => (
         <Course key={course.id} course={course} />
-      ))}
+        ))
+      }
     </div>
   )
 }
