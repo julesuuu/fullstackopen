b860ced2c92d3285db34701a733344354a9e7086
Julius De Perio <julesrdeperio@gmail.com>
2026-01-02 01:42:28 +0800

    added id for the inital value of persons array

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', id: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
