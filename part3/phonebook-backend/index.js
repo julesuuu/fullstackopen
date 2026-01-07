2e6235c79b8528293ad2c6e1c67ae60788b80325
Julius De Perio <julesrdeperio@gmail.com>
2026-01-07 13:33:47 +0800

    3.9 Phonebook backend step 9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 app.use(morgan("tiny"));
+const cors = require("cors");
+app.use(cors());
 
 const date = new Date();
 
