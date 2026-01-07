9ca0a45a04827846bb585aa28c4e39f42576ffd6
Julius De Perio <julesrdeperio@gmail.com>
2026-01-07 14:24:20 +0800

    updated dist

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -109,7 +109,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server Running on ${PORT}`);
 });
