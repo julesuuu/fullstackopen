1b4f9f497a3f1776771258fd7bf26061f620c372
Julius De Perio <julesrdeperio@gmail.com>
2026-01-21 16:37:19 +0800

    5.6 Blog List Frontend, step 6

--- part5/bloglist-frontend/src/components/BlogForm.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -1,35 +1,47 @@
-const BlogForm = ({ 
-  onSubmit, 
-  handleTitleChange, 
-  handleAuthorChange, 
-  handleUrlChange, 
-  title, 
-  author, 
-  url 
-}) => {
+import { useState } from 'react'
+
+const BlogForm = ({ createBlog }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const addBlog = (event) => {
+    event.preventDefault()
+  }
+
+  createBlog({
+    title: title,
+    author: author,
+    url: url
+  })
+
+  setTitle('')
+  setAuthor('')
+  setUrl('')
+
   return (
     <div>
       <h2>create new</h2>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={addBlog}>
         <div>
           title:
           <input
             value={title}
-            onChange={handleTitleChange}
+            onChange={({ target }) => setTitle(target.value)}
           />
         </div>
         <div>
           author:
           <input
             value={author}
-            onChange={handleAuthorChange}
+            onChange={({ target }) => setTitle(target.value)}
           />
         </div>
         <div>
           url:
           <input
             value={url}
-            onChange={handleUrlChange}
+            onChange={({ target }) => setTitle(target.value)}
           />
         </div>
         <button type="submit">create</button>
