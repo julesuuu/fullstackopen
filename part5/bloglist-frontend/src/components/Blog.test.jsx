48a44feb1f6c6240a3151b2b4047182a487d1af1
Julius De Perio <julesrdeperio@gmail.com>
2026-01-23 20:22:06 +0800

    5.14: Blog List Tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 import { test, expect } from 'vitest'
 
@@ -23,4 +24,34 @@ test('renders title and author', () => {
 
   const url = screen.queryByText('https://fullstackopen.com/')
   expect(url).toBeNull()
+
+  screen.debug(author)
+})
+
+test('clicking the view button displays url and likes', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Arto Hellas',
+    url: 'https://fullstackopen.com/',
+    likes: 10,
+    user: {
+      username: 'jules',
+      name: 'Jules'
+    }
+  }
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const url = screen.getByText('https://fullstackopen.com/')
+  const likes = screen.queryByText('likes 10', { exact: false })
+
+  expect(url).toBeDefined()
+  expect(likes).toBeDefined()
+
+  screen.debug(button)
 })
