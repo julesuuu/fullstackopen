9b22c963fbe7ab87760df927da09a1dea91da497
Julius De Perio <julesrdeperio@gmail.com>
2026-01-29 20:30:34 +0800

    Exercise 6.21

--- part6/query-anecdotes-main/src/requests.js
+++ part6/query-anecdotes-main/src/requests.js
@@ -4,7 +4,23 @@ export const getAnecdotes = async () => {
   const response = await fetch(baseUrl)
   
   if (!response.ok) {
-    throw new Error('Failed to fetch anecdotes')
+    throw new Error('failed to fetch anecdotes')
   }
+  return await response.json()
+}
+
+export const createAnecdote = async (newAnecdote) => {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newAnecdote)
+  }
+
+  const response = await fetch(baseUrl, options)
+
+  if (!response.ok) {
+    throw new Error('failed to create anecdote')
+  }
+
   return await response.json()
 }
