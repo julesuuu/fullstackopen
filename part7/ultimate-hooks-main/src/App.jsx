1821f8c8591c89180d800dbb7c68fab35bb5d7e5
Julius De Perio <julesrdeperio@gmail.com>
2026-02-01 11:48:01 +0800

    7.8 clear input fields

--- part7/ultimate-hooks-main/src/App.jsx
+++ part7/ultimate-hooks-main/src/App.jsx
@@ -8,10 +8,15 @@ const useField = (type) => {
     setValue(event.target.value)
   }
 
+  const reset = () => {
+    setValue('')
+  }
+
   return {
     type,
     value,
-    onChange
+    onChange,
+    reset
   }
 }
 
@@ -47,32 +52,39 @@ const App = () => {
   const name = useField('text')
   const number = useField('text')
 
+  const { reset: resetContent, ...contentProps } = content
+  const { reset: resetName, ...nameProps } = name
+  const { reset: resetNumber, ...numberProps } = number
+
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
   const handleNoteSubmit = (event) => {
     event.preventDefault()
     noteService.create({ content: content.value })
+    resetContent()
   }
 
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
+    resetName()
+    resetNumber()
   }
 
   return (
     <div>
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
-        <input {...content} />
+        <input {...contentProps} />
         <button>create</button>
       </form>
       {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
-        number <input {...number} />
+        name <input {...nameProps} /> <br/>
+        number <input {...numberProps} />
         <button>create</button>
       </form>
       {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
