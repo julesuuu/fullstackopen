e343fa28f6c06f601144778cc33ebce52207bd28
Julius De Perio <julesrdeperio@gmail.com>
2026-02-06 16:04:35 +0800

    8.4: Books of an author

--- part8/index.js
+++ part8/index.js
@@ -28,21 +28,7 @@ let authors = [
   },
 ]
 
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
-*/
-
-let books = [
+let books = [ 
   {
     title: 'Clean Code',
     published: 2008,
@@ -94,10 +80,6 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
-
 const typeDefs = /* GraphQL */  `
   type Book {
     title: String!
@@ -115,7 +97,7 @@ const typeDefs = /* GraphQL */  `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String) : [Book!]!  
     allAuthors: [Author!]!
   }
 `
@@ -124,7 +106,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      return books.filter(b => b.author === args.author)
+    },
     allAuthors: () => authors
   },
   Author: {
