f9b2dcc281efa8ab046efc89aa58b367f09d52ff
Julius De Perio <julesrdeperio@gmail.com>
2026-02-06 16:37:00 +0800

    8.6: Adding a book

--- part8/index.js
+++ part8/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { GraphQLError } = require('graphql')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -97,8 +98,17 @@ const typeDefs = /* GraphQL */  `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String, genre: String) : [Book!]!  
+    allBooks(author: String, genre: String) : [Book!]!
     allAuthors: [Author!]!
+  }
+  
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ) : Book
   }
 `
 
@@ -124,6 +134,18 @@ const resolvers = {
   Author: {
     bookCount: (root) => {
       return books.filter(b => b.author === root.name).length
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.find(a => a.name === args.author)) {
+        const newAuthor = { name: args.author, id: uuid() }
+        authors = authors.concat(newAuthor)
+      }
+
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
     }
   }
 }
