dafd5eae99c868d020d876c4dd3824cb259ab2e0
Julius De Perio <julesrdeperio@gmail.com>
2026-04-07 04:10:04 +0800

    9.3 bmiCalculator

--- part9/healthapp/bmiCalculator.ts
+++ part9/healthapp/bmiCalculator.ts
@@ -1,7 +1,25 @@
-const calculateBmi = (height: number, weight: number) => {
+interface BmiValues {
+  value1: number;
+  value2: number;
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+const calculateBmi = (height: number, weight: number): string => {
   const heightInMeters = height / 100;
   const bmi = weight / (heightInMeters * heightInMeters); // Math.pow(heightInMeters, 2)
-  console.log("🚀 ~ :4 ~ calculateBmi ~ bmi:", bmi);
 
   if (bmi < 18.5) {
     return "Underweight range";
@@ -14,4 +32,13 @@ const calculateBmi = (height: number, weight: number) => {
   }
 };
 
-console.log(calculateBmi(180, 74));
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  console.log(calculateBmi(value1, value2));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
