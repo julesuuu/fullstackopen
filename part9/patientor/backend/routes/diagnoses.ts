32dbf083be00021c879a3fcb9728af6569e5aa11
Julius De Perio <julesrdeperio@gmail.com>
2026-04-10 01:31:16 +0800

    9.11 Patientor backend, step3

---
+++ part9/patientor/backend/routes/diagnoses.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import diagnosesService from "../services/diagnosesService.ts";
+
+const router = express.Router();
+
+router.get("/", (_req, res) => {
+  const data = diagnosesService.getDiagnoses();
+  res.send(data);
+});
+
+export default router;
