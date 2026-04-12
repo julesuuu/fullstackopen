9fe506618c6bb8d8e1d690eae3271e0282b70c61
Julius De Perio <julesrdeperio@gmail.com>
2026-04-13 03:51:38 +0800

    9.22 Flight diaries, step4

--- part9/flightdiaries/frontend/src/App.tsx
+++ part9/flightdiaries/frontend/src/App.tsx
@@ -53,28 +53,127 @@ const App = () => {
       {errorMessage && <h3 style={{ color: "red" }}> {errorMessage} </h3>}
       <form onSubmit={diaryCreation}>
         <label htmlFor="date">date:</label>
-        <input type="text" value={newDiary.date} onChange={(e) => setNewDiary({ ...newDiary, date: e.target.value })} />
-        <br />
-        <label htmlFor="visibility">visibility:</label>
         <input
-          type="text"
-          value={newDiary.visibility}
+          style={{ marginLeft: "10px" }}
+          type="date"
+          value={newDiary.date}
+          onChange={(e) => setNewDiary({ ...newDiary, date: e.target.value })}
+        />
+        <br />
+        <label style={{ marginRight: "10px" }} htmlFor="visibility">
+          visibility:
+        </label>
+        <label htmlFor="great">great</label>
+        <input
+          type="radio"
+          name="visibility"
+          id="great"
+          value="great"
+          checked={newDiary.visibility === "great"}
+          onChange={(e) => setNewDiary({ ...newDiary, visibility: e.target.value as Visibility })}
+        />
+        <label style={{ marginLeft: "10px" }} htmlFor="good">
+          good
+        </label>
+        <input
+          type="radio"
+          name="visibility"
+          id="good"
+          value="good"
+          checked={newDiary.visibility === "good"}
+          onChange={(e) => setNewDiary({ ...newDiary, visibility: e.target.value as Visibility })}
+        />
+        <label style={{ marginLeft: "10px" }} htmlFor="ok">
+          ok
+        </label>
+        <input
+          type="radio"
+          name="visibility"
+          id="ok"
+          value="ok"
+          checked={newDiary.visibility === "ok"}
+          onChange={(e) => setNewDiary({ ...newDiary, visibility: e.target.value as Visibility })}
+        />
+        <label style={{ marginLeft: "10px" }} htmlFor="poor">
+          poor
+        </label>
+        <input
+          type="radio"
+          name="visibility"
+          id="poor"
+          value="poor"
+          checked={newDiary.visibility === "poor"}
           onChange={(e) => setNewDiary({ ...newDiary, visibility: e.target.value as Visibility })}
         />
         <br />
-        <label htmlFor="weather">weather:</label>
+        <label style={{ marginRight: "10px" }} htmlFor="weather">
+          weather:
+        </label>
+        <label style={{ marginLeft: "10px" }} htmlFor="sunny">
+          sunny
+        </label>
         <input
-          type="text"
-          value={newDiary.weather}
+          type="radio"
+          name="weather"
+          id="sunny"
+          value="sunny"
+          checked={newDiary.weather === "sunny"}
+          onChange={(e) => setNewDiary({ ...newDiary, weather: e.target.value as Weather })}
+        />
+        <label style={{ marginLeft: "10px" }} htmlFor="rainy">
+          rainy
+        </label>
+        <input
+          type="radio"
+          name="weather"
+          id="rainy"
+          value="rainy"
+          checked={newDiary.weather === "rainy"}
+          onChange={(e) => setNewDiary({ ...newDiary, weather: e.target.value as Weather })}
+        />
+        <label style={{ marginLeft: "10px" }} htmlFor="cloudy">
+          cloudy
+        </label>
+        <input
+          type="radio"
+          name="weather"
+          id="cloudy"
+          value="cloudy"
+          checked={newDiary.weather === "cloudy"}
+          onChange={(e) => setNewDiary({ ...newDiary, weather: e.target.value as Weather })}
+        />
+        <label style={{ marginLeft: "10px" }} htmlFor="stormy">
+          stormy
+        </label>
+        <input
+          type="radio"
+          name="weather"
+          id="stormy"
+          value="stormy"
+          checked={newDiary.weather === "stormy"}
+          onChange={(e) => setNewDiary({ ...newDiary, weather: e.target.value as Weather })}
+        />
+        <label style={{ marginLeft: "10px" }} htmlFor="windy">
+          windy
+        </label>
+        <input
+          type="radio"
+          name="weather"
+          id="windy"
+          value="windy"
+          checked={newDiary.weather === "windy"}
           onChange={(e) => setNewDiary({ ...newDiary, weather: e.target.value as Weather })}
         />
         <br />
-        <label htmlFor="comment">comment:</label>
+        <label style={{ marginRight: "10px" }} htmlFor="comment">
+          comment:
+        </label>
         <input
           type="text"
           value={newDiary.comment}
           onChange={(e) => setNewDiary({ ...newDiary, comment: e.target.value })}
         />
+        <br />
         <button type="submit">add</button>
       </form>
       <h2>Diary Entries</h2>
